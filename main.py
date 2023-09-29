cb4977cfed62411e08d76f09e65a9447bfbc8621
LarsPasman <95089151+LarsPasman@users.noreply.github.com>
2023-09-29 10:44:36 +0000

    Agent met puntensysteem

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from bke import EvaluationAgent, start, can_win
-
-#De agent zoekt nu naar een positie waarin hij kan winnen
-class MijnSpeler(EvaluationAgent):
-    def evaluate(self, board, my_symbol, opponent_symbol):
-        getal = 1
-        if can_win(board, opponent_symbol):
-            getal = getal - 1000
-        return getal
+from bke import MLAgent, is_winner, opponent, start
  
-mijn_speler = MijnSpeler()
-start(player_o=mijn_speler)
+ #De agent leert nu door middel van een puntensysteem, hij krijgt punten als hij wint en verliest punten als hij verliest.
+class MyAgent(MLAgent):
+    def evaluate(self, board):
+        if is_winner(board, self.symbol):
+            reward = 1
+        elif is_winner(board, opponent[self.symbol]):
+            reward = -1
+        else:
+            reward = 0
+        return reward
+    
+ 
+my_agent = MyAgent()
+#aanroepen agent die punten wilt verzamelen.
+
+start(player_x=my_agent)
+# Nu word er een een agent aangeroepen die gebruik maakt van het puntensysteem waarin hij streeft naar punten.
