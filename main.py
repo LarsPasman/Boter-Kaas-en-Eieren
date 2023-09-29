5284eacaa4dd1e0a5f52f558993bf5957109c426
LarsPasman <95089151+LarsPasman@users.noreply.github.com>
2023-09-29 10:30:05 +0000

    Agent herkent in welke positie hij kan winnen

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from bke import EvaluationAgent, start
+from bke import EvaluationAgent, start, can_win
 
-#De speler doet geen random zetten meer maar hij evalueert het bord.
+#De agent zoekt nu naar een positie waarin hij kan winnen
 class MijnSpeler(EvaluationAgent):
     def evaluate(self, board, my_symbol, opponent_symbol):
         getal = 1
-        if board[4] == my_symbol:
-            getal = getal + 5
+        if can_win(board, opponent_symbol):
+            getal = getal - 1000
         return getal
  
 mijn_speler = MijnSpeler()
