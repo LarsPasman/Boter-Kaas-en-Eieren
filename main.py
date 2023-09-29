e342fea40482b43f330ccec08e254aa3b169ece1
LarsPasman <95089151+LarsPasman@users.noreply.github.com>
2023-09-29 10:50:21 +0000

    Spelen tegen opgeslagen agent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from bke import MLAgent, is_winner, opponent, train, save
+from bke import MLAgent, is_winner, opponent, load, start
  
-class SlimmeAgent(MLAgent):
+class MyAgent(MLAgent):
     def evaluate(self, board):
         if is_winner(board, self.symbol):
             reward = 1
@@ -8,12 +8,11 @@ class SlimmeAgent(MLAgent):
             reward = -1
         else:
             reward = 0
-        return reward    
- 
-my_agent = SlimmeAgent()
- 
-train(my_agent, 3000)
-# Hier gaat de agent 3000 keer het spel spelen. waardoor die steeds beter begrijpt hoe hij de meeste punten kan krijgen en dus steeds beter wordt in het spel.
+        return reward
 
-# Hier wordt de agent opgeslagen in een map genaamd 'SlimmeAgent'
-save(my_agent, 'SlimmeAgent')
+#Hier wordt de agent die we net hebben opgeslagen in de aparte map teruggeroepen om tegen te spelen
+my_agent = load('SlimmeAgent') 
+my_agent.learning = False
+
+#Hier word voor player x nu de slimmeagent gebruikt die we net getraingt hebben 
+start(player_x=my_agent)
